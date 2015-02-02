a4b70dce296a1c590ca6e21f4a6014215f6100ed
Fernando Magalhães <fm4lloc@gmail.com>
2015-02-02 13:17:49 -0200

    Update ct-main-window.c

--- src/gui/ct-main-window.c
+++ src/gui/ct-main-window.c
@@ -50,7 +50,7 @@ void create_main_window (struct window_main_widgets *wm)
 	gtk_box_pack_start (GTK_BOX (wm->vbox1), wm->menubar1, FALSE, TRUE, 1);
 
 	wm->vpaned1 = gtk_vpaned_new ();
-    gtk_paned_set_position (GTK_PANED (wm->vpaned1), 200);
+	gtk_paned_set_position (GTK_PANED (wm->vpaned1), 200);
 	gtk_container_add (GTK_CONTAINER (wm->vbox1), wm->vpaned1);
 
 	wm->hpaned1 = gtk_hpaned_new ();
@@ -59,21 +59,21 @@ void create_main_window (struct window_main_widgets *wm)
 
 	wm->treeview1 = create_tree_view ();
 	wm->scrolled1 = create_scrolled_window_with_viewport (wm->treeview1);
-    gtk_container_add (GTK_CONTAINER (wm->hpaned1), wm->scrolled1);
+	gtk_container_add (GTK_CONTAINER (wm->hpaned1), wm->scrolled1);
 
 	wm->textview1 = gtk_text_view_new ();
 	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (wm->textview1),
 			GTK_WRAP_CHAR);
 
 	wm->scrolled2 = create_scrolled_window_with_viewport (wm->textview1);
-    gtk_container_add (GTK_CONTAINER (wm->vpaned1), wm->scrolled2);
+	gtk_container_add (GTK_CONTAINER (wm->vpaned1), wm->scrolled2);
 
 	wm->textview2 = gtk_text_view_new ();
 	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (wm->textview2),
 			GTK_WRAP_CHAR );
 
 	wm->scrolled3 = create_scrolled_window_with_viewport (wm->textview2);
-    gtk_container_add (GTK_CONTAINER(wm->hpaned1), wm->scrolled3);
+	gtk_container_add (GTK_CONTAINER(wm->hpaned1), wm->scrolled3);
 
 	wm->buttonbox1 = gtk_hbutton_box_new ();
 	gtk_button_box_set_layout (GTK_BUTTON_BOX (wm->buttonbox1),
@@ -81,15 +81,15 @@ void create_main_window (struct window_main_widgets *wm)
 
 	wm->button1 = gtk_button_new_with_label ("Encode");
 	gtk_container_add (GTK_CONTAINER (wm->buttonbox1), wm->button1);
-    gtk_signal_connect (GTK_OBJECT (wm->button1), "clicked",
-    		GTK_SIGNAL_FUNC (encode_cb), wm);
+	gtk_signal_connect (GTK_OBJECT (wm->button1), "clicked",
+			GTK_SIGNAL_FUNC (encode_cb), wm);
 
 	wm->button2 = gtk_button_new_with_label ("Decode");
 	gtk_container_add (GTK_CONTAINER (wm->buttonbox1), wm->button2);
-    gtk_signal_connect (GTK_OBJECT (wm->button2), "clicked",
-    		GTK_SIGNAL_FUNC (decode_cb), wm);
+	gtk_signal_connect (GTK_OBJECT (wm->button2), "clicked",
+			GTK_SIGNAL_FUNC (decode_cb), wm);
 
-    gtk_box_pack_start (GTK_BOX (wm->vbox1), wm->buttonbox1, FALSE, TRUE, 1);
+	gtk_box_pack_start (GTK_BOX (wm->vbox1), wm->buttonbox1, FALSE, TRUE, 1);
 
 	wm->statusbar1 = gtk_statusbar_new ();
 	gtk_box_pack_end (GTK_BOX (wm->vbox1), wm->statusbar1, FALSE, TRUE, 1);
